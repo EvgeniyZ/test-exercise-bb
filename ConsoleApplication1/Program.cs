4c5f91e0d02a37c817cbac45c956594e51758114
Eugene Zakharov <rocketjump07@gmail.com>
2016-09-13 23:31:08 +0600

    add solution for test exercise.

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = File.Open("in.txt", FileMode.Open);
+            var cookiesCollections = new List<string[]>();
+            using (var streamReader = new StreamReader(input))
+            {
+                var collectionsCount = streamReader.ReadLine();
+                var collectionsCountAsNumber = int.Parse(collectionsCount);
+                for (int i = 0; i < collectionsCountAsNumber; i++)
+                {
+                    streamReader.ReadLine();
+                    var cookies = streamReader.ReadLine();
+                    cookiesCollections.Add(cookies.Split(' '));
+                }
+            }
+            input.Close();
+            var caseNumber = 1;
+            foreach (var cookieCollection in cookiesCollections)
+            {
+                var binarySum = "0";
+                var actualSum = 0;
+                var minimum = int.Parse(cookieCollection[0]);
+                foreach (var cookie in cookieCollection)
+                {
+                    var weight = int.Parse(cookie);
+                    if (weight < minimum)
+                    {
+                        minimum = weight;
+                    }
+                    binarySum = Sum(Convert.ToString(weight, 2), binarySum);
+                    actualSum += weight;
+                }
+                var condition = Convert.ToInt32(binarySum, 2);
+                if (condition != 0)
+                {
+                    WriteToOutput(caseNumber, "NO");
+                }
+                else
+                {
+                    WriteToOutput(caseNumber, (actualSum - minimum).ToString());
+                }
+                caseNumber++;
+            }
+        }
+
+        public static string Sum(string first, string second)
+        {
+            var shortestValue = first.Length < second.Length ? first : second;
+            var longestValue = first.Length < second.Length ? second : first;
+            var commonPart = longestValue.Substring(longestValue.Length - shortestValue.Length);
+            var result = new StringBuilder();
+            for (var i = 0; i < commonPart.Length; i++)
+            {
+                result.Append(shortestValue[i] != commonPart[i] ? '1' : '0');
+            }
+            var greaterPart = longestValue.Substring(0, longestValue.Length - shortestValue.Length);
+
+            return greaterPart + result;
+        }
+
+        private static void WriteToOutput(int caseNumber, string value)
+        {
+            var output = File.Open("out.txt", FileMode.OpenOrCreate);
+            output.Seek(0, SeekOrigin.End);
+            using (var streamWriter = new StreamWriter(output))
+            {
+                streamWriter.WriteLine(string.Format("Case #{0}: {1}", caseNumber, value));
+            }
+            output.Close();
+        }
+    }
+}
---
+++ ConsoleApplication1/Tests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace ConsoleApplication1
+{
+    class Tests
+    {
+        [Test]
+        public void TestStupidBinarySum()
+        {
+            var first = 5;
+            var second = 4;
+            var result = Program.Sum(Convert.ToString(first, 2), Convert.ToString(second, 2));
+            Assert.AreEqual(Convert.ToInt32(result, 2), 1);
+            first = 7;
+            second = 9;
+            result = Program.Sum(Convert.ToString(first, 2), Convert.ToString(second, 2));
+            Assert.AreEqual(Convert.ToInt32(result, 2), 14);
+            first = 50;
+            second = 10;
+            result = Program.Sum(Convert.ToString(first, 2), Convert.ToString(second, 2));
+            Assert.AreEqual(Convert.ToInt32(result, 2), 56);
+        }
+    }
+}
